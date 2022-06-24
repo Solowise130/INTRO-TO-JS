dc0e776d5df91039d8e8b665447b2a68640588cf
Solomon Iniodu <102917931+Solowise130@users.noreply.github.com>
2022-06-24 07:48:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myName = "Solomon";
+const myName = "Solomon Iniodu";
 const myHeight = "189 cm";
 const myCountry = "Nigeria";
 alert("My name is " + myName+ "\nMy height is " + myHeight + "\nMy country is " + myCountry)
